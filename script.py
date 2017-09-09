a44e42131a0564d24c410227668eb0582e9b0fae
Rubin Thomas <rubin.thomas@stonybrook.edu>
2017-09-09 16:11:54 -0400

    sped up a little using lxml

--- script.py
+++ script.py
@@ -1,6 +1,8 @@
 import bs4
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
+import numpy as np
+from bs4 import SoupStrainer
 
 class Chord:
 	def __init__(self, index = []):
@@ -15,39 +17,35 @@ def scrape():
 	uClient.close()
 
 	#creats page
-	page_soup = soup(page_html, "html.parser")
-
-	artists = page_soup.findAll('td', {"class" : "alphabet"})
-	for div in artists:
-		links = div.findAll('a')
-		for a in links:
-			my_url = a['href']
-			uClient = uReq(my_url)
-			page_html = uClient.read()
-			uClient.close()
-
-			page_soup = soup(page_html, "html.parser")
-			songs = page_soup.findAll('ul', {"id" : "results"})
-			for song in songs:
-				second = song.findAll('a', {"class" : "ta"})
-				for a in second:
-					print(a['href'])
+	strainer = SoupStrainer('p')
+	artists = soup(page_html, "lxml", parse_only = strainer)
+	for a in artists.find_all('a'):
+		my_url = a['href']
+		uClient = uReq(my_url)
+		page_html = uClient.read()
+		uClient.close()
 	
-
-	
-	"""
-	#now lets find where the chords are
-	chords = page_soup.findAll('u')
-
-	#iterates over chords and adds everything to a list
-	myList = list()
-	for i in chords: 
-		myList.append(i.getText())
-	
-	print(myList)
+		page_soup = soup(page_html, "html.parser")
+		songs = page_soup.findAll('ul', {"id" : "results"})
+		for song in songs:
+			second = song.findAll('a', {"class" : "ta"})
+			for a in second:
+				my_url = a['href']
+				uClient = uReq(my_url)
+				page_html = uClient.read()
+				uClient.close()
+				page_soup = soup(page_html, "html.parser")
+				#now lets find where the chords are
+				chords = page_soup.findAll('u')
+				#iterates over chords and adds everything to a list
+				print("done")
+				"""myList = list()
+				for i in chords: 
+					myList.append(i.getText())
+					print(myList)"""
    
-	#if not i in S CHECKING IF SOMETHINGISNOTINS
-	"""
+	#if not i in S CHECKING IF SOMETHIN
+
 
 def main():
 	scrape()
